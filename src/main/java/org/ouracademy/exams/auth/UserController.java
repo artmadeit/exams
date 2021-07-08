5004fa242c86156115a1d7c13bc95ea487244dd5
artmadeit <this.arthur.email@gmail.com>
2021-07-08 12:25:23 -0500

    refactor: remove register user

--- src/main/java/org/ouracademy/exams/auth/UserController.java
+++ src/main/java/org/ouracademy/exams/auth/UserController.java
@@ -5,17 +5,12 @@ import java.security.Principal;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 
-import org.ouracademy.exams.auth.RegisterUserService.UserNameExistException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
@@ -26,23 +21,11 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/auth")
 public class UserController {
 
-    private RegisterUserService registerService;
     private SignInService signInService;
-
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(UserNameExistException.class)
-    public void registerException() {
-        // do nothing
-    }
-
-    @PostMapping("/register")
-    public UserDetails register(@RequestBody @Valid RegisterUserService.UserInput input) {
-        return this.registerService.run(input);
-    }
 
     @GetMapping("/verify")
     public ResponseEntity<UserAccount> verify(@RequestParam @NotBlank String name) {
-        return ResponseEntity.of(this.registerService.verify(name));
+        return ResponseEntity.of(this.signInService.verify(name));
     }
 
     @PostMapping("/login")
