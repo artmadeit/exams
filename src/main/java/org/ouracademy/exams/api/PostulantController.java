6d8007eb0ac0d571938694b69c1d97eb00f4c527
artmadeit <this.arthur.email@gmail.com>
2021-07-10 10:42:34 -0500

    refactor: remove unused attribute

--- src/main/java/org/ouracademy/exams/api/PostulantController.java
+++ src/main/java/org/ouracademy/exams/api/PostulantController.java
@@ -1,21 +1,14 @@
 package org.ouracademy.exams.api;
 
-import java.security.Principal;
-
-import org.ouracademy.exams.auth.UserAccount;
 import org.ouracademy.exams.domain.Postulant;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.AllArgsConstructor;
-
 @RestController
 @RequestMapping("/postulant")
-@AllArgsConstructor
 public class PostulantController {
-    PostulantRepository repository;
 
     @GetMapping("/me")
     public Postulant currentPostulant(@AuthenticationPrincipal Postulant postulant) {
