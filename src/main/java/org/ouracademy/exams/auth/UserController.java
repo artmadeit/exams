99966ca24254a8939cbf1f88c56e4c470c1f0dc3
artmadeit <this.arthur.email@gmail.com>
2021-07-26 23:17:04 -0500

    refactor: remove sample i18n code

--- src/main/java/org/ouracademy/exams/auth/UserController.java
+++ src/main/java/org/ouracademy/exams/auth/UserController.java
@@ -1,12 +1,9 @@
 package org.ouracademy.exams.auth;
 
 import java.security.Principal;
-import java.util.Locale;
 
 import javax.validation.Valid;
 
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,17 +20,8 @@ public class UserController {
 
     private SignInService signInService;
 
-    MessageSource messageSource;
-
     @PostMapping("/login")
     public String login(@RequestBody @Valid SignInService.LoginInput input) {
-
-        var hola = LocaleContextHolder.getLocale();
-        // var hola = Locale.getDefault();
-        System.out.println("aaaaa" + hola);
-        System.out.println(messageSource.getMessage("welcome.text", new Object[] {}, hola));
-
-
         return signInService.run(input);
     }
 
