8a55d7a5e3612f14938d071910cdfc031c594b88
artmadeit <this.arthur.email@gmail.com>
2021-07-26 23:42:09 -0500

    feat: authentication using i18n

--- src/main/java/org/ouracademy/exams/GlobalExceptionHandler.java
+++ src/main/java/org/ouracademy/exams/GlobalExceptionHandler.java
@@ -2,20 +2,24 @@ package org.ouracademy.exams;
 
 import java.util.Locale;
 
-import org.ouracademy.exams.utils.BadArgumentsException;
 import org.ouracademy.exams.utils.OuracademyException;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.NativeWebRequest;
 import org.zalando.problem.Problem;
+import org.zalando.problem.Status;
+import org.zalando.problem.spring.web.advice.security.AuthenticationAdviceTrait;
 
 import lombok.AllArgsConstructor;
 
 @ControllerAdvice
 @AllArgsConstructor
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler implements AuthenticationAdviceTrait {
     final MessageSource messageSource;
 
     @ExceptionHandler(OuracademyException.class)
@@ -29,5 +33,18 @@ public class GlobalExceptionHandler {
             .build();
 
         return new ResponseEntity<>(problem, HttpStatus.valueOf(ex.getStatus().getStatusCode()));
+    }
+
+    @Override
+    public ResponseEntity<Problem> handleAuthentication(AuthenticationException ex, NativeWebRequest request) {
+        String errorMessage = messageSource.getMessage("unauthorized", null, LocaleContextHolder.getLocale());
+
+        var problem = Problem.builder()
+            .withDetail(errorMessage)
+            .withTitle(ex.getMessage())
+            .withStatus(Status.UNAUTHORIZED)
+            .build();
+            
+        return new ResponseEntity<>(problem, HttpStatus.UNAUTHORIZED);
     }
 }
