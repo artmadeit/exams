dabb1c059d827ae9d2890ed7ee7e87178f9338b3
artmadeit <this.arthur.email@gmail.com>
2021-08-01 22:55:47 -0500

    feat: optimize login / retrieve examPart

--- src/main/java/org/ouracademy/exams/auth/UserRepository.java
+++ src/main/java/org/ouracademy/exams/auth/UserRepository.java
@@ -3,8 +3,10 @@ package org.ouracademy.exams.auth;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends JpaRepository<UserAccount, Long> {
+    @Transactional(readOnly = true)
     public Optional<UserAccount> findByName(String name);
 
 }
--- src/main/java/org/ouracademy/exams/domain/structure/ExamPartRepository.java
+++ src/main/java/org/ouracademy/exams/domain/structure/ExamPartRepository.java
@@ -4,9 +4,11 @@ import java.util.Collection;
 import java.util.List;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ExamPartRepository extends PagingAndSortingRepository<ExamPart, Long> {
 
+    @Transactional(readOnly = true)
     List<ExamPart> findByTitleIn(Collection<String> titles);
     
 }
