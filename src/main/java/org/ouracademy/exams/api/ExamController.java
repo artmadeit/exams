d24fd8b57944ea54b0ffb32c0b2cd652cf9bdd78
artmadeit <this.arthur.email@gmail.com>
2021-08-09 11:32:51 -0500

    feat: get one exam

--- src/main/java/org/ouracademy/exams/api/ExamController.java
+++ src/main/java/org/ouracademy/exams/api/ExamController.java
@@ -1,14 +1,20 @@
 package org.ouracademy.exams.api;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 
+import org.ouracademy.exams.domain.structure.Exam;
 import org.ouracademy.exams.domain.structure.ExamPart;
 import org.ouracademy.exams.domain.structure.ExamPartInfoResponse;
 import org.ouracademy.exams.domain.structure.ExamRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +30,28 @@ import lombok.Setter;
 public class ExamController {
     ExamRepository repository;
 
+
+    @Getter
+    @AllArgsConstructor
+    public static class ExamPartResponse2 {
+        Long id;
+        String title;
+        String content;
+        ExamPart.Type type;
+        List<ExamPartResponse2> childs;
+
+        public static ExamPartResponse2 recursive(ExamPart exam) {
+            var childs = exam.getChilds().stream().map(ExamPartResponse2::recursive).collect(Collectors.toList());
+            return new ExamPartResponse2(exam.getId(), exam.getTitle(), exam.getContent(), exam.getType(), childs);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ExamPartResponse2> findById(@PathVariable Long id) {
+        return ResponseEntity.of(
+            repository.findById(id).map(ExamPartResponse2::recursive)
+        );
+    }
 
     @GetMapping
     public Page<ExamPartInfoResponse> getAll(final Pageable pageable) {
