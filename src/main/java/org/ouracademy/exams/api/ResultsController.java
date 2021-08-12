ec5d851d315377f47ced646d580f9321cf019d13
artmadeit <this.arthur.email@gmail.com>
2021-08-12 17:47:29 -0500

    feat: initial idea search exams

--- src/main/java/org/ouracademy/exams/api/ResultsController.java
+++ src/main/java/org/ouracademy/exams/api/ResultsController.java
@@ -2,15 +2,16 @@ package org.ouracademy.exams.api;
 
 import java.util.Optional;
 
+import javax.persistence.criteria.JoinType;
+
 import org.ouracademy.exams.domain.DateTimeRange;
 import org.ouracademy.exams.domain.PostulantExam;
 import org.ouracademy.exams.domain.PostulantExamRepository;
-import org.ouracademy.exams.domain.event.ExamEvent;
 import org.ouracademy.exams.domain.event.ExamEventRepository;
 import org.ouracademy.exams.domain.postulant.Postulant;
-import org.ouracademy.exams.utils.NotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,8 +49,27 @@ public class ResultsController {
         @RequestParam Optional<String> dni,
         @RequestParam Optional<String> programCode) {
         
-        var examEvent = examEvents.findById(examEventId)
-            .orElseThrow(() -> new NotFoundException(ExamEvent.class, examEventId));
-        return postulantExamRepository.findByEvent(examEvent, pageable).map(PostulantExamSummary::toDTO);
+        var spec = postulantExamsofExamEvent(examEventId, dni, programCode);
+
+        return postulantExamRepository.findAll(spec, pageable).map(PostulantExamSummary::toDTO);
+    }
+
+    public static Specification<PostulantExam> postulantExamsofExamEvent(
+        Long examEventId, Optional<String> optionalDni, Optional<String> optionalProgramCode) {
+        return (root, query, cb) -> {
+            // select e.*
+            // from postulant_exam e right join postulant p
+            // where e.event_id = :eventId
+            // and p.dni = :dni and p.programCode = :programCode
+            
+            var exam = root;
+            var p = exam.join("postulant", JoinType.RIGHT);
+            
+            return cb.and(
+                cb.equal(exam.get("event").get("id"), examEventId),
+                optionalDni.map(dni -> cb.equal(p.get("dni"), dni)).orElse(null),
+                optionalProgramCode.map(programCode -> cb.equal(p.get("programCode"), programCode)).orElse(null)
+            );
+        };
     }
 }
--- src/main/java/org/ouracademy/exams/domain/PostulantExamRepository.java
+++ src/main/java/org/ouracademy/exams/domain/PostulantExamRepository.java
@@ -4,13 +4,11 @@ import java.util.Optional;
 
 import org.ouracademy.exams.domain.event.ExamEvent;
 import org.ouracademy.exams.domain.postulant.Postulant;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface PostulantExamRepository extends PagingAndSortingRepository<PostulantExam, Long> {
+public interface PostulantExamRepository extends PagingAndSortingRepository<PostulantExam, Long>, JpaSpecificationExecutor<PostulantExam> {
 
     Optional<PostulantExam> findByPostulantAndEvent(Postulant postulant, ExamEvent examEvent);
 
-    Page<PostulantExam> findByEvent(ExamEvent examEvent, Pageable pageable);
 }
