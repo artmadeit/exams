c844879764f0b2dd7b005407648f0573222e3ea5
artmadeit <this.arthur.email@gmail.com>
2021-08-13 13:19:52 -0500

    feat: lob like psql text

--- src/main/java/org/ouracademy/exams/domain/structure/ExamPart.java
+++ src/main/java/org/ouracademy/exams/domain/structure/ExamPart.java
@@ -12,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OrderColumn;
@@ -48,6 +49,7 @@ public class ExamPart {
     String title;
     
     @Setter
+    @Lob
     String content;
 
     @Enumerated(EnumType.STRING)
