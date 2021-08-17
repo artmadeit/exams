a064c155a220783a282b575660e2b9962b4dcb16
artmadeit <this.arthur.email@gmail.com>
2021-08-17 16:15:16 -0500

    feat: add ratios & stats from old project

---
+++ seeder/stats.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+path = "data/resultados_27_03_2021_sin_puntaje_negativo.xlsx"
+columns = ['DNI', 'Cod. Postulante', 'Apellidos y Nombres','UPG', 'Programa', 'Puntaje',
+              'Fecha inicio', 'Fecha final']
+
+df = pd.read_excel(path, skiprows=1, usecols = columns)
+df = df.replace(regex=r'^\*', value='')
+
+# eliminar demo
+# { "codigo_upg": { $eq: "1111" } }
+
+df = df[df["UPG"] != 1111]
+
+df["aprobo?"] = df["Puntaje"] >= 55
+
+df["noDioExamen"] = df["Fecha inicio"].isna()
+
+print("total", len(df))
+# df.to_csv("resultados_resumido.csv")
+
+aprobados = df[df["aprobo?"]]
+print("aprobados", len(aprobados))
+desaprobados = df[~df["aprobo?"]]
+print("desaprobados", len(desaprobados))
+print("no dio examen:", len(df[df["noDioExamen"]]))
+print(df[["Puntaje"]].describe())
+
+# print(df.head())
