9526e76bae1b791da7ada30fc9d9d96cf01df377
artmadeit <this.arthur.email@gmail.com>
2021-08-24 23:01:15 -0500

    feat: add thresholds

--- stress.js
+++ stress.js
@@ -2,22 +2,23 @@ import http from "k6/http";
 import { check, sleep } from "k6";
 
 export let options = {
-  vus: 2,
-  duration: "10s"
-  // stages: [
-  //   // { duration: "1m", target: 1 }, // below normal load
-  //   // { duration: "5m", target: 100 },
-  //   // { duration: "2m", target: 200 }, // normal load
-  //   // { duration: "5m", target: 200 },
-  //   // { duration: "2m", target: 300 }, // around the breaking point
-  //   // { duration: "5m", target: 300 },
-  //   // { duration: "2m", target: 400 }, // beyond the breaking point
-  //   // { duration: "5m", target: 400 },
-  //   // { duration: "10m", target: 0 }, // scale down. Recovery stage.
-  // ],
+  thresholds: {
+    http_req_duration: ['p(95) < 1000', 'p(99.9) < 2000'],
+  },
+  stages: [
+    { duration: "1m", target: 1 }, // below normal load
+    // { duration: "5m", target: 100 },
+    // { duration: "2m", target: 200 }, // normal load
+    // { duration: "5m", target: 200 },
+    // { duration: "2m", target: 300 }, // around the breaking point
+    // { duration: "5m", target: 300 },
+    // { duration: "2m", target: 400 }, // beyond the breaking point
+    // { duration: "5m", target: 400 },
+    // { duration: "10m", target: 0 }, // scale down. Recovery stage.
+  ],
 };
 
-const BASE_URL = "https://prod-spring-exams-api.herokuapp.com";
+const BASE_URL = "https://qa-spring-exams-api.herokuapp.com";
 const eventExamId = 1;
 
 
