cb56ab9246e07b26e92cff36fbe303d45091c94e
artmadeit <this.arthur.email@gmail.com>
2021-08-27 16:34:37 -0500

    feat: ratio ingreso x facultad

--- seeder/stats.py
+++ seeder/stats.py
@@ -12,18 +12,32 @@ df = df.replace(regex=r'^\*', value='')
 
 df = df[df["UPG"] != 1111]
 
-df["aprobo?"] = df["Puntaje"] >= 55
+df["esta_aprobado"] = df["Puntaje"] >= 55
 
-df["noDioExamen"] = df["Fecha inicio"] == "-"
+df["es_ausente"] = df["Fecha inicio"] == "-"
 
 print("total", len(df))
 # df.to_csv("resultados_resumido.csv")
 
-aprobados = df[df["aprobo?"]]
+aprobados = df[df["esta_aprobado"]]
 print("aprobados", len(aprobados))
-desaprobados = df[~df["aprobo?"]]
+desaprobados = df[~df["esta_aprobado"]]
 print("desaprobados", len(desaprobados))
-print("no dio examen:", len(df[df["noDioExamen"]]))
+print("Ausentes:", len(df[df["es_ausente"]]))
 print(df[["Puntaje"]].describe())
 
 # print(df.head())
+
+result = df.groupby("UPG").aggregate({
+    "DNI": ["count"],
+    "es_ausente": ["count"],
+    "esta_aprobado": ["count"]
+})
+
+# result["Inscritos"] = df_grouped["DNI"].count()
+# result["Ausentes"] = df_grouped["es_ausente"].count()
+# result["Postulantes"] = result["Inscritos"] - result["Ausentes"] 
+# result["Ingresantes"] = result["esta_aprobado"].count()
+# result["Ratio de aprobados"] = result["Ingresantes"] / result["Postulantes"]
+# result["Ratio de asistencia"] = result["Postulantes"] / result["Inscritos"]
+# print(result)
