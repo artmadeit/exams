fdf326a949faa0cd45f40a41f0521738e2af65b7
artmadeit <this.arthur.email@gmail.com>
2021-08-30 01:55:37 -0500

    fix: cacheable error

--- src/main/java/org/ouracademy/exams/domain/PostulantQuestionRepository.java
+++ src/main/java/org/ouracademy/exams/domain/PostulantQuestionRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface PostulantQuestionRepository extends PagingAndSortingRepository<PostulantQuestion, Long> {
 	
-	@Cacheable("questions")
+	//@Cacheable("questions")
 	// TODO: evaluate @Query("select pq from PostulantQuestion pq join fetch pq.alternativeReferences where pq.number = :questionNumber and pq.postulantExam.id = :examId")
     public Optional<PostulantQuestion> findByNumberAndPostulantExam_Id(Integer questionNumber, Long examId);    
  
